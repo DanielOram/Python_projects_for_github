5942e8fd1ed3dda33341ab1db8ecc0e2baf73c17
Danoram <danoram1@xtra.co.nz>
2015-07-25 13:59:12 +1200

    minor additions

--- Python_list_comprehension/List_Comprehension_Examples.py
+++ Python_list_comprehension/List_Comprehension_Examples.py
@@ -46,6 +46,10 @@ from random import randint
 #generate random input for this example
 randomize = lambda a,b: [list(set([randint(0,9) for x in range(randint(1,a))])) for x in range(b)]
 
+#compress a list function
+compf = lambda x: [j for i in x for j in i]
+
+#compress a 2D list into a single list
 compress_to_unique = lambda x: list(set([j for i in x for j in i]))
 fill = lambda lists: [[f for f in compress_to_unique(lists) if not f in e] for e in lists]
 r = randomize(6,10)
